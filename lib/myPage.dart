00cf5df36fcef78f8536b5e498597005bbfaa065
JaeIkLee730 <21200574@handong.edu>
2019-05-23 10:14:11 +0900

    sign in/out with google

--- lib/myPage.dart
+++ lib/myPage.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class MyPage extends StatefulWidget {
 
+  final FirebaseUser user;
+  final FirebaseAuth auth;
+  final GoogleSignIn googleSignIn;
+
+  MyPage({Key key,
+    @required this.user,
+    @required this.auth,
+    @required this.googleSignIn,
+  });
+
   @override
-  _MyPageState createState() => _MyPageState();
+  _MyPageState createState() {
+    print(user.displayName);
+
+    return _MyPageState(
+      user: user,
+      auth: auth,
+      googleSignIn: googleSignIn,
+    );
+  }
 
 }
 
 class _MyPageState extends State<MyPage> {
+
+  final FirebaseUser user;
+  final FirebaseAuth auth;
+  final GoogleSignIn googleSignIn;
+
+  _MyPageState({
+    Key key,
+    @required this.user,
+    @required this.auth,
+    @required this.googleSignIn,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +53,7 @@ class _MyPageState extends State<MyPage> {
               Navigator.pushNamed(context, '/home');
             },
           ),
+          Text("MYPAGE", style: TextStyle(fontSize: 50), ),
         ],
       ),
       backgroundColor: Colors.indigo,
